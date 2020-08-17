71275c692ef8f5c1587a05977c9d84d16076429d
Fischbacher Markus <fischbacher.markus@gmail.com>
2020-08-17 10:28:53 +0200

    fix server

--- server-pgsql/centos/docker-entrypoint.sh
+++ server-pgsql/centos/docker-entrypoint.sh
@@ -186,11 +186,7 @@ check_db_connect_postgresql() {
         export PGSSLKEY=${ZBX_DBTLSKEYFILE}
     fi
 
-    while true :
-    do
-        psql --host ${DB_SERVER_HOST} --port ${DB_SERVER_PORT} --username ${DB_SERVER_ROOT_USER} --list --quiet 1>/dev/null 2>&1 && break
-        psql --host ${DB_SERVER_HOST} --port ${DB_SERVER_PORT} --username ${DB_SERVER_ROOT_USER} --list --dbname ${DB_SERVER_DBNAME} --quiet 1>/dev/null 2>&1 && break
-
+    while [ ! "$(psql --host ${DB_SERVER_HOST} --port ${DB_SERVER_PORT} --username ${DB_SERVER_ROOT_USER} --dbname ${DB_SERVER_DBNAME} --list --quiet 2>/dev/null)" ]; do
         echo "**** PostgreSQL server is not available. Waiting $WAIT_TIMEOUT seconds..."
         sleep $WAIT_TIMEOUT
     done
